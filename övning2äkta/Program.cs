773e83acb4437019b0a2da19ddead75a66eaa5b5
JamesGill12345 <112473466+JamesGill12345@users.noreply.github.com>
2024-03-20 12:35:26 +0100

    Add files via upload

---
+++ övning2äkta/Program.cs
@@ -0,0 +1,53 @@
+﻿
+//punkt 1
+
+string[] leksaker = { "Lego", "Docka", "leksaksbil" };
+
+foreach (string lekssak in leksaker)
+{
+    Console.WriteLine(leksaker);
+}
+
+//punkt 2
+
+string[] klasskamrater = { "hampus", "neo", "oliver", "hector", "yuriy" };
+
+// punkt 3
+
+int[] numbers = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+
+//punkt 4
+
+
+
+for (int i = 0; i < klasskamrater.Length; i++)
+{
+    int r1 = Random.Shared.Next(0, 5);
+    int r2 = Random.Shared.Next(0, 10);
+
+    Console.WriteLine($"{klasskamrater[i]} ger {klasskamrater[i]} {numbers[i]} i betyg");
+}
+
+// punkt 5
+
+List<string> cities = new List<string>();
+
+// punkt 6
+bool run;
+run = true;
+while(run = true)
+{
+    Console.WriteLine("Skriv något");
+    string input = Console.ReadLine();
+    cities.Add(input);
+    if (input == "exit")
+    {
+        run = false;
+    }
+}
+
+// punkt 7
+
+Console.WriteLine(cities);
+
